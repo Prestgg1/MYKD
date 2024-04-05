f6d70c76aad00f66f2ec937d4a434b16226882f8
Prestgg1 <prestgg12@gmail.com>
2024-04-05 21:50:00 +0400

    Eposide 2

--- assets/scripts/Script.js
+++ assets/scripts/Script.js
@@ -3,3 +3,11 @@ window.addEventListener('mousemove',(e)=>{
     cursor.style.left = -10+e.pageX + 'px'
     cursor.style.top = -10+e.pageY + 'px'
 })
+document.querySelector('.fa-bars').addEventListener('click',()=>{
+    document.querySelector('.rightnav').classList.toggle('navactive')
+
+})
+
+document.querySelector('.fa-xmark').addEventListener('click',()=>{
+    document.querySelector('.rightnav').classList.remove('navactive')
+})
