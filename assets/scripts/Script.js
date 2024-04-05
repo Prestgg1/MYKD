54ca7adb6f1b34299ffa50fe94923c354544918c
Prestgg1 <prestgg12@gmail.com>
2024-04-05 23:25:44 +0400

    Eposide 3

--- assets/scripts/Script.js
+++ assets/scripts/Script.js
@@ -3,11 +3,23 @@ window.addEventListener('mousemove',(e)=>{
     cursor.style.left = -10+e.pageX + 'px'
     cursor.style.top = -10+e.pageY + 'px'
 })
-document.querySelector('.fa-bars').addEventListener('click',()=>{
+
+let bars = document.querySelector('.fa-bars')
+let rightnav = document.querySelector('.rightnav')
+bars.addEventListener('click',()=>{
     document.querySelector('.rightnav').classList.toggle('navactive')
 
 })
 
-document.querySelector('.fa-xmark').addEventListener('click',()=>{
-    document.querySelector('.rightnav').classList.remove('navactive')
+document.querySelector('.exit').addEventListener('click',()=>{
+    rightnav.classList.remove('navactive')
 })
+
+document.onclick = (e)=>{
+    if(bars.contains(e.target) || rightnav.contains(e.target)){
+        
+    }
+    else{
+        rightnav.classList.remove('navactive')
+    }
+}
