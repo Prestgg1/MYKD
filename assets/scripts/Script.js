6b14cf777aaa53090bdc9e9b3ea57f24c820c27d
Prestgg1 <prestgg12@gmail.com>
2024-04-16 23:20:29 +0400

    Quick Fix

--- assets/scripts/Script.js
+++ assets/scripts/Script.js
@@ -4,43 +4,8 @@ window.addEventListener('mousemove',(e)=>{
     cursor.style.top = -10+e.pageY + 'px'
 })
 
-let bars = document.querySelector('.fa-bars')
-let rightnav = document.querySelector('.rightnav')
-let rightnavpc = document.querySelector('.rightnavpc')
-bars.addEventListener('click',()=>{
-    document.querySelector('.rightnav').classList.toggle('navactive')
-
+document.querySelectorAll('.container img.quarter').forEach((e)=>{
+    e.style.top =`${Math.floor(Math.random()*100)}%`
+    e.style.left =`${Math.floor(Math.random()*100)}%`
 })
-
-document.querySelector('.logo-and-exit .exit').addEventListener('click',()=>{
-    rightnav.classList.remove('navactive')
-})
-
-/* rightnavmobile */
-
-document.onclick = (e)=>{
-    if(bars.contains(e.target) || rightnav.contains(e.target)){
-        
-    }
-    else{
-        rightnav.classList.remove('navactive')
-    }
-    if(document.querySelector('.burger').contains(e.target) || rightnavpc.contains(e.target)){
-
-    }
-    else{
-        rightnavpc.style.right = '-100%'
-    }
-} 
-
-/* rightnavmobile */
-
-
-/* rightnavpc */
-document.querySelector('.burger').addEventListener('click',()=>{
-    rightnavpc.style.right = 0
-})
-document.querySelector('.rightnavpc .logo-and-exit .exit').onclick = ()=>{
-    rightnavpc.style.right = "-100%"
-}
 
