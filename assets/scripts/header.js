5bcf0efc8bb6fd999418754a9098151964281931
Prestgg1 <prestgg12@gmail.com>
2024-06-09 23:17:12 +0400

    Right Header Fixed

--- assets/scripts/header.js
+++ assets/scripts/header.js
@@ -1,5 +1,7 @@
-const header = document.querySelector('header');                          
-header.insertAdjacentHTML('beforeend', `
+const header = document.querySelector("header");
+header.insertAdjacentHTML(
+  "beforeend",
+  `
 <div id="search-sheet">
         <div class="exit">
             <svg stroke="currentColor" fill="rgb(69,248,130)" stroke-width="0" viewBox="0 0 512 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M19.75 14.438c59.538 112.29 142.51 202.35 232.28 292.718l3.626 3.75.063-.062c21.827 21.93 44.04 43.923 66.405 66.25-18.856 14.813-38.974 28.2-59.938 40.312l28.532 28.53 68.717-68.717c42.337 27.636 76.286 63.646 104.094 105.81l28.064-28.06c-42.47-27.493-79.74-60.206-106.03-103.876l68.936-68.938-28.53-28.53c-11.115 21.853-24.413 42.015-39.47 60.593-43.852-43.8-86.462-85.842-130.125-125.47-.224-.203-.432-.422-.656-.625C183.624 122.75 108.515 63.91 19.75 14.437zm471.875 0c-83.038 46.28-154.122 100.78-221.97 161.156l22.814 21.562 56.81-56.812 13.22 13.187-56.438 56.44 24.594 23.186c61.802-66.92 117.6-136.92 160.97-218.72zm-329.53 125.906l200.56 200.53c-4.36 4.443-8.84 8.793-13.405 13.032L148.875 153.53l13.22-13.186zm-76.69 113.28l-28.5 28.532 68.907 68.906c-26.29 43.673-63.53 76.414-106 103.907l28.063 28.06c27.807-42.164 61.758-78.174 104.094-105.81l68.718 68.717 28.53-28.53c-20.962-12.113-41.08-25.5-59.937-40.313 17.865-17.83 35.61-35.433 53.157-52.97l-24.843-25.655-55.47 55.467c-4.565-4.238-9.014-8.62-13.374-13.062l55.844-55.844-24.53-25.374c-18.28 17.856-36.602 36.06-55.158 54.594-15.068-18.587-28.38-38.758-39.5-60.625z"></path></svg>
@@ -121,7 +123,9 @@ header.insertAdjacentHTML('beforeend', `
 <div class="rightnavcontainer">
     <div class="logo-and-exit">
         <div class="logo">
-            <img src="https://themedox.com/mykd/wp-content/themes/mykd/assets/img/logo/logo.png" style="max-height: 40px" alt="Logo">
+        <a href="/index.html"><img src="https://themedox.com/mykd/wp-content/themes/mykd/assets/img/logo/logo.png" style="max-height: 40px" alt="Logo"></a>
+            
+
         </div>
         <div class="exit">
             <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 512 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M19.75 14.438c59.538 112.29 142.51 202.35 232.28 292.718l3.626 3.75.063-.062c21.827 21.93 44.04 43.923 66.405 66.25-18.856 14.813-38.974 28.2-59.938 40.312l28.532 28.53 68.717-68.717c42.337 27.636 76.286 63.646 104.094 105.81l28.064-28.06c-42.47-27.493-79.74-60.206-106.03-103.876l68.936-68.938-28.53-28.53c-11.115 21.853-24.413 42.015-39.47 60.593-43.852-43.8-86.462-85.842-130.125-125.47-.224-.203-.432-.422-.656-.625C183.624 122.75 108.515 63.91 19.75 14.437zm471.875 0c-83.038 46.28-154.122 100.78-221.97 161.156l22.814 21.562 56.81-56.812 13.22 13.187-56.438 56.44 24.594 23.186c61.802-66.92 117.6-136.92 160.97-218.72zm-329.53 125.906l200.56 200.53c-4.36 4.443-8.84 8.793-13.405 13.032L148.875 153.53l13.22-13.186zm-76.69 113.28l-28.5 28.532 68.907 68.906c-26.29 43.673-63.53 76.414-106 103.907l28.063 28.06c27.807-42.164 61.758-78.174 104.094-105.81l68.718 68.717 28.53-28.53c-20.962-12.113-41.08-25.5-59.937-40.313 17.865-17.83 35.61-35.433 53.157-52.97l-24.843-25.655-55.47 55.467c-4.565-4.238-9.014-8.62-13.374-13.062l55.844-55.844-24.53-25.374c-18.28 17.856-36.602 36.06-55.158 54.594-15.068-18.587-28.38-38.758-39.5-60.625z"></path></svg>
@@ -139,13 +143,13 @@ header.insertAdjacentHTML('beforeend', `
     <li><label for="home"> Home <input type="checkbox" id="home"></label>
     <ul>
         <hr>
-        <li> HOME ONE </li>
+        <li><a href="/index.html">HOME ONE</a></li>
         <hr>
-        <li>HOME TWO</li>
+        <li><a href="/index-2.html">HOME TWO</a> </li>
     </ul>
     </li>
     <hr>
-    <li>ABOUT US</li>
+    <li><a href="/pages/about.html">ABOUT US</a></li>
     <hr>
     <li><label for="tournament"> TOURNAMENT <input type="checkbox" id="tournament"></label>
         <ul>
@@ -186,7 +190,7 @@ header.insertAdjacentHTML('beforeend', `
         </ul>
     </li>
     <hr>
-    <li>CONTACT</li>
+    <li><a href="/pages/contact.html">CONTACT US</a></li>
     <hr>
 </ul>
 </div>
@@ -198,73 +202,78 @@ header.insertAdjacentHTML('beforeend', `
     <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 448 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M446.7 98.6l-67.6 318.8c-5.1 22.5-18.4 28.1-37.3 17.5l-103-75.9-49.7 47.8c-5.5 5.5-10.1 10.1-20.7 10.1l7.4-104.9 190.9-172.5c8.3-7.4-1.8-11.5-12.9-4.1L117.8 284 16.2 252.2c-22.1-6.9-22.5-22.1 4.6-32.7L418.2 66.4c18.4-6.9 34.5 4.1 28.5 32.2z"></path></svg>
 </div>
 </div>
-`)
-let bars = document.querySelector('.fa-bars')
-let rightnav = document.querySelector('.rightnav')
-let rightnavpc = document.querySelector('.rightnavpc')
-document.querySelector('#search-sheet .exit').addEventListener('click',()=>{
-    document.querySelector('#search-sheet').style.top = "-100%"
-})
+`
+);
+let bars = document.querySelector(".fa-bars");
+let rightnav = document.querySelector(".rightnav");
+let rightnavpc = document.querySelector(".rightnavpc");
+document.querySelector("#search-sheet .exit").addEventListener("click", () => {
+  document.querySelector("#search-sheet").style.top = "-100%";
+});
 
 /* Quarter */
 
-document.onclick = (e)=>{
-    if(bars.contains(e.target) || rightnav.contains(e.target)){
-        
-    }
-    else{
-        rightnav.classList.remove('navactive')
-    }
-    if(document.querySelector('.burger').contains(e.target) || rightnavpc.contains(e.target)){
+document.onclick = (e) => {
+  if (bars.contains(e.target) || rightnav.contains(e.target)) {
+  } else {
+    rightnav.classList.remove("navactive");
+  }
+  if (
+    document.querySelector(".burger").contains(e.target) ||
+    rightnavpc.contains(e.target)
+  ) {
+  } else {
+    rightnavpc.style.right = "-100%";
+  }
+};
+bars.addEventListener("click", () => {
+  document.querySelector(".rightnav").classList.toggle("navactive");
+});
 
-    }
-    else{
-        rightnavpc.style.right = '-100%'
-    }
-} 
-bars.addEventListener('click',()=>{
-    document.querySelector('.rightnav').classList.toggle('navactive')
-
-})
-
-document.querySelector('.rightnavcontainer .logo-and-exit .exit').addEventListener('click',()=>{
-    rightnav.classList.remove('navactive')
-})
-
-/* rightnavmobile */
-
-
+document
+  .querySelector(".rightnavcontainer .logo-and-exit .exit")
+  .addEventListener("click", () => {
+    rightnav.classList.remove("navactive");
+  });
 
 /* rightnavmobile */
 
+/* rightnavmobile */
 
 /* rightnavpc */
-document.querySelector('.burger').addEventListener('click',()=>{
-    rightnavpc.style.right = 0
-})
-document.querySelector('.rightnavpc .logo-and-exit .exit').onclick = ()=>{
-    rightnavpc.style.right = "-100%"
-}
+document.querySelector(".burger").addEventListener("click", () => {
+  rightnavpc.style.right = 0;
+});
+document.querySelector(".rightnavpc .logo-and-exit .exit").onclick = () => {
+  rightnavpc.style.right = "-100%";
+};
 AOS.init();
 
 let lastScrollY;
-window.addEventListener('scroll',()=>{
+window.addEventListener("scroll", () => {
   let currentScroll = window.scrollY;
-  if(currentScroll<=87){
-    document.querySelector('header').classList.remove('header-hidden')
-    document.querySelector('header').classList.remove('header-visible')
-    document.querySelector('.upper').classList.remove('upper-show')
-}
-if(currentScroll>=lastScrollY && !document.querySelector('header').classList.contains('header-hidden') ){
-    document.querySelector('header').classList.add('header-hidden')
-    document.querySelector('header').classList.remove('header-visible')
-}
-if(currentScroll<lastScrollY && !document.querySelector('header').classList.contains('header-visible') && currentScroll>=87){
-    document.querySelector('header').classList.add('header-visible')
-    document.querySelector('header').classList.remove('header-hidden')
-}
-if(currentScroll>=100){
-  document.querySelector('.upper').classList.add('upper-show')
-}
-lastScrollY=currentScroll;
-})
+  if (currentScroll <= 87) {
+    document.querySelector("header").classList.remove("header-hidden");
+    document.querySelector("header").classList.remove("header-visible");
+    document.querySelector(".upper").classList.remove("upper-show");
+  }
+  if (
+    currentScroll >= lastScrollY &&
+    !document.querySelector("header").classList.contains("header-hidden")
+  ) {
+    document.querySelector("header").classList.add("header-hidden");
+    document.querySelector("header").classList.remove("header-visible");
+  }
+  if (
+    currentScroll < lastScrollY &&
+    !document.querySelector("header").classList.contains("header-visible") &&
+    currentScroll >= 87
+  ) {
+    document.querySelector("header").classList.add("header-visible");
+    document.querySelector("header").classList.remove("header-hidden");
+  }
+  if (currentScroll >= 100) {
+    document.querySelector(".upper").classList.add("upper-show");
+  }
+  lastScrollY = currentScroll;
+});
